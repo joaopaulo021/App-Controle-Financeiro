9abc3e4bb4070363235caadf489ad92cb55cebb5
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-05-17 01:36:01 -0300

    Add header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  height: 150px;
+  text-align: center;
+  background: teal;
+  background: linear-gradient(to bottom, #2980b9, #6ab2e8);
+box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  
+`;
+
+export const Header = styled.h1 ``;
+
+export const Title = styled.div `
+  padding-top: 20px;
+  color: #fff;
+  
+`;
